e37c082ba7236ef9c40ea208990e0cde56312133
Paramonov Vladimir <44196011+VovaParamonov@users.noreply.github.com>
2019-05-19 20:11:22 +0600

    replace all components to functions

---
+++ precache-manifest.966eed62ce7e0316d6ec8e3b7b9d6788.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f72513deef67cc084214",
+    "url": "/static/css/main.768f829b.chunk.css"
+  },
+  {
+    "revision": "f72513deef67cc084214",
+    "url": "/static/js/main.f72513de.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "414e00536aa5e5368e63",
+    "url": "/static/js/2.414e0053.chunk.js"
+  },
+  {
+    "revision": "95eae398612560a43d8b00ec872736bf",
+    "url": "/static/media/Eni-min.95eae398.svg"
+  },
+  {
+    "revision": "41ffd4535e397b4104b4b435f5e1bdc8",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.426dd03e803edb282660673ff42a5fdd.js"
+  "/precache-manifest.966eed62ce7e0316d6ec8e3b7b9d6788.js"
 );
 
 workbox.clientsClaim();
