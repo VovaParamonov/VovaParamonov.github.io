3dd5fcdd07a3ffb2651090d0c695c7580cb18d85
Paramonov Vladimir <44196011+VovaParamonov@users.noreply.github.com>
2020-04-21 21:52:58 +0600

    qweawd

---
+++ precache-manifest.4b5658afa205c26a225c59986f495794.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52c643ced91177874843f952b34852ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2adced7b42b456a52d2c",
+    "url": "/static/css/main.bf28f5e7.chunk.css"
+  },
+  {
+    "revision": "51771eb7b262b78ef4f8",
+    "url": "/static/js/2.6fcd482e.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/static/js/2.6fcd482e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2adced7b42b456a52d2c",
+    "url": "/static/js/main.8402de3d.chunk.js"
+  },
+  {
+    "revision": "a4f8bcbab9b69aecc45e",
+    "url": "/static/js/runtime-main.eaee7ad6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
