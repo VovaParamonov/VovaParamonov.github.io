9b223ac710a368b7687ca0315d52fc37ec2fd7c8
Paramonov Vladimir <44196011+VovaParamonov@users.noreply.github.com>
2020-04-25 01:29:04 +0600

    set site

---
+++ precache-manifest.46f5177ccbf076bccb48e44096671fc4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "040f3c3baa4ec7f3c2c4705d81aca846",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b16c5e6146566bf521ff",
+    "url": "/static/css/main.c7581106.chunk.css"
+  },
+  {
+    "revision": "6bb359337f8adb9e42d4",
+    "url": "/static/js/2.66d49ef2.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/static/js/2.66d49ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b16c5e6146566bf521ff",
+    "url": "/static/js/main.0a1aeb19.chunk.js"
+  },
+  {
+    "revision": "a4f8bcbab9b69aecc45e",
+    "url": "/static/js/runtime-main.eaee7ad6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6eccd4357213f01ac58339a4b38964ef.js"
+  "/precache-manifest.46f5177ccbf076bccb48e44096671fc4.js"
 );
 
 self.addEventListener('message', (event) => {
